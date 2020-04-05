ccefc22217bb6d2f8b45e3c6090f38bafe7e8d7e
Josimar Gomes <josimar.gomes@superlogica.com>
2020-04-05 20:24:41 -0300

    add request permission Android

--- src/components/QRCodeReader/index.js
+++ src/components/QRCodeReader/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { PermissionsAndroid } from 'react-native'
 import { CameraKitCameraScreen } from 'react-native-camera-kit';
 import {
     COLOR_SUPORTE_PRIMARY,
@@ -7,6 +8,36 @@ import {
 } from '../../design-stystem';
 
 export default function QRCodeReader({ onReader }) {
+
+    const [hasPermission, setPermission] = useState(false);
+
+
+    async function requestCameraPermission() {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+                title: 'Acesso a câmera',
+                message:
+                'Precisamos acessar a câmera do seu dispositvo.',
+                buttonNeutral: 'Pergunte-me depois',
+                buttonNegative: 'Negar',
+                buttonPositive: 'Permitir',
+            },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            setPermission(true);
+        }
+    }
+
+    useEffect(() => {
+        requestCameraPermission();
+
+    }, [])
+
+    if(!hasPermission) {
+        return null;
+    }
+
     return (
         <CameraKitCameraScreen
             showFrame={true}
